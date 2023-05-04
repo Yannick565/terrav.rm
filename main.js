9580989e424a0b9e98ce023e7afc243d1076c736
YannickAP <yannickkindt@hotmail.be>
2023-05-04 09:47:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,171 @@
+import './style.css';
+import * as THREE from 'three';
+import {ObjectControls} from 'threeJS-object-controls';
+//import * as CANNON from 'cannon-es';
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({canvas:document.querySelector('.bg')});
+
+/*
+const world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0), // m/s²
+})
+*/
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(40);
+var controls = new ObjectControls(camera, renderer.domElement);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.SphereGeometry( 15, 50, 50 );
+const material = new THREE.MeshStandardMaterial({color: 0x2e6930, wireframe:false});
+const sphere = new THREE.Mesh(geometry, material,);
+scene.add(sphere);
+
+const geometryD = new THREE.SphereGeometry( 15, 50, 50 );
+const materialD = new THREE.MeshStandardMaterial({color: 0x2e6930, wireframe:false});
+const sphereD = new THREE.Mesh(geometryD, materialD);
+scene.add(sphereD);
+
+//const globeTexture = new THREE.TextureLoader().load( "./assets/waterTexture.jpg" );
+const geometryO = new THREE.SphereGeometry( 15.1, 400, 400 );
+const materialO = new THREE.MeshStandardMaterial({color: 0x6495ED, wireframe:false} );
+const OuterSphere = new THREE.Mesh(geometryO, materialO,);
+scene.add(OuterSphere);
+
+OuterSphere.userData.ignoreRaycast = true;
+
+const light = new THREE.DirectionalLight( 0xffffCf, 1.2);
+light.position.set( 10, 10, 30 );
+scene.add( light );
+
+const gridhelper = new THREE.GridHelper(200, 50);
+//scene.add(gridhelper)
+
+/*
+function onDocumentMouseMove(event) {
+  const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+  const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  camera.position.x = mouseX * 20;
+  camera.position.y = mouseY * 20;
+
+  camera.lookAt(scene.position);
+}
+*/
+
+const raycaster = new THREE.Raycaster();
+const clickMouse = new THREE.Vector2();
+const vector3 = new THREE.Vector3();
+const MAX_CLICK_DISTANCE = 2.2
+
+window.addEventListener('click', event => {
+  clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(clickMouse, camera);
+  const found = raycaster.intersectObjects(scene.children.filter(obj => obj !== OuterSphere));
+  if (found.length > 0 && (found[0].object).geometry) {
+      const mesh = found[0].object
+      if (mesh === OuterSphere){
+        return;
+      } 
+      const geometry = mesh.geometry
+      const point = found[0].point
+      const isSphereD = mesh === sphereD;
+      const isSphere = mesh === sphere;
+      
+      for (let i = 0; i  < geometry.attributes.position.count; i++) {
+          vector3.setX(geometry.attributes.position.getX(i))
+          vector3.setY(geometry.attributes.position.getY(i))
+          vector3.setZ(geometry.attributes.position.getZ(i))
+          const toWorld = mesh.localToWorld(vector3)
+          const distance = point.distanceTo(toWorld)
+          if (distance < MAX_CLICK_DISTANCE) {
+              if (isSphereD) {
+                  geometry.attributes.position.setY(i, geometry.attributes.position.getY(i) - (MAX_CLICK_DISTANCE - distance) / 2);
+                  console.log("create land down")
+              } 
+              if (isSphere){
+                  geometry.attributes.position.setY(i, geometry.attributes.position.getY(i) + (MAX_CLICK_DISTANCE - distance) / 2);
+                  console.log("create land up")
+              }
+          }
+      }
+      geometry.computeVertexNormals()
+      geometry.attributes.position.needsUpdate = true
+  }
+});
+
+document.addEventListener('mousemove',(event) => {
+  const rotationSpeed = 0.00004 ;
+  sphere.rotation.y += (event.clientX - window.innerWidth / 2) * rotationSpeed;
+  sphere.rotation.x += (event.clientY - window.innerHeight / 2) * rotationSpeed;
+  sphereD.rotation.y += (event.clientX - window.innerWidth / 2) * rotationSpeed;
+  sphereD.rotation.x += (event.clientY - window.innerHeight / 2) * rotationSpeed;
+});
+
+
+function addStars(){
+  const geometry = new THREE.SphereGeometry(0.04, 2, 2);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(140));
+  star.position.set(x, y, z);
+  star.position.z = -1;
+  scene.add(star);
+}
+
+Array(2000).fill().forEach(addStars);
+
+scene.background = new THREE.Color( 0x1F1921 );
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+render();
+
+controls.enableHorizontalRotation();
+controls.enableVerticalRotation();
+//controls.setRotationSpeed(0.1);
+controls.setObjectToMove(sphere, sphereD);
+controls.disableZoom();
+
+
+
+
+
+//dev tools
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "KeyF") {
+    document.querySelector(".bg").style.cursor = "none";
+    console.log("cursor off")
+  } else if (event.code === "KeyG") {
+    document.querySelector(".bg").style.cursor = "default";
+    console.log("cursor on")
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "KeyR") {
+    location.reload();  
+  }
+});
+
+console.log(
+  "DEV TOOLS",
+  "",
+  "---",
+  "",
+  "Cursor off = F",
+  "",
+  "Cursor on = G",
+  "",
+  "Reset = R"
+);
